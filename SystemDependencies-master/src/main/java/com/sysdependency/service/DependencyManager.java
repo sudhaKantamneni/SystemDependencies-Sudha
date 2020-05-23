863521d8f7f1485a34e9c8f4e24fd6365120ec3d
sudhaKantamneni <65807648+sudhaKantamneni@users.noreply.github.com>
2020-05-23 21:50:40 +0530

    Update DependencyManager.java

--- SystemDependencies-master/src/main/java/com/sysdependency/service/DependencyManager.java
+++ SystemDependencies-master/src/main/java/com/sysdependency/service/DependencyManager.java
@@ -4,6 +4,10 @@ import com.sysdependency.model.Program;
 import java.util.Set;
 
 public interface DependencyManager {
+     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
     Set<Program> list() throws ServiceException;
 
     void depend(Program source, Set<Program> dest) throws ServiceException;
