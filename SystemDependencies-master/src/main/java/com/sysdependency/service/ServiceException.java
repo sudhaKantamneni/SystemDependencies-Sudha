e5fd5fd44af0fe58f8f403819ca28bbe68654a6b
sudhaKantamneni <65807648+sudhaKantamneni@users.noreply.github.com>
2020-05-23 21:51:59 +0530

    Update ServiceException.java

--- SystemDependencies-master/src/main/java/com/sysdependency/service/ServiceException.java
+++ SystemDependencies-master/src/main/java/com/sysdependency/service/ServiceException.java
@@ -1,7 +1,14 @@
 package com.sysdependency.service;
 
+/*
+Service Exception Class
+*/
 public class ServiceException extends Exception {
 
+       /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
     public ServiceException(String message) {
         super(message);
     }
